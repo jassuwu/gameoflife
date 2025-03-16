7f58da44b30cfa98487e9630c47def06fd7f93e2
jassuwu <jass@mande.network>
2025-03-16 14:36:05 +0530

    golang: Grid definition and init

--- golang/main.go
+++ golang/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	"golang.org/x/term"
 )
 
 // STEPS:
 // [x] Get the terminal dimensions
-// [ ] Define the structure of the grid and initialize randomly
+// [x] Define the structure of the grid and initialize randomly
 // [ ] Write the next grid computation with the GOL rules.
 // [ ] Infinitely loop the GOL generations
 // [ ] VOILA! CONWAY's GAME OF LIFE BABYYYY
@@ -19,8 +20,45 @@ func assert(condition bool, message string) {
 	}
 }
 
-func main() {
+const (
+	DEAD  = ' '
+	ALIVE = '#'
+)
+
+type Grid struct {
+	R, C   int
+	values [][]rune
+}
+
+func NewGrid() *Grid {
 	COLS, ROWS, err := term.GetSize(0)
 	assert(err == nil, "error when getting the terminal size")
-	fmt.Println(ROWS, COLS)
+	values := make([][]rune, ROWS)
+	for r := range ROWS {
+		row := make([]rune, COLS)
+		for c := range COLS {
+			// Randomly initialize the cell as ALIVE or DEAD.
+			if rand.Intn(10) <= 8 {
+				row[c] = DEAD
+			} else {
+				row[c] = ALIVE
+			}
+		}
+		values[r] = row
+	}
+	return &Grid{R: ROWS, C: COLS, values: values}
+}
+
+func (g *Grid) Print() {
+	for r := 0; r < g.R; r++ {
+		for c := 0; c < g.C; c++ {
+			fmt.Printf("%c", g.values[r][c])
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	grid := NewGrid()
+	grid.Print()
 }
