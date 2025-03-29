fbdf019fdf3885e35a325c1be6e1551a9dc6f516
jassuwu <karthickprnv@gmail.com>
2025-03-29 15:54:18 +0530

    python: init grid

--- python/gameoflife.py
+++ python/gameoflife.py
@@ -5,6 +5,21 @@
 # 4. infinite loop and run :)
 
 import os
+import random
 
-COLS, ROWS = os.get_terminal_size()
-print(COLS, ROWS)
+DEAD                   = " "
+ALIVE                  = "■" # Use something blocky: ■ or #
+
+class GOL:
+    def __init__(self):
+        COLS, ROWS = os.get_terminal_size()
+        self.C = COLS
+        self.R = ROWS
+        self.grid = [[ALIVE if random.random() > 0.8 else DEAD for _ in range(COLS)] for _ in range(ROWS)]
+
+    def print(self):
+        for r in range(self.R):
+            print("".join(self.grid[r]))
+
+gol = GOL()
+gol.print()
