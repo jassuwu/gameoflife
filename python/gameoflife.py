556c772800fd2027ba2ce5b4c6cb7e10ed62abfc
jassuwu <karthickprnv@gmail.com>
2025-03-29 15:34:21 +0530

    chore: steps in python

--- python/gameoflife.py
+++ python/gameoflife.py
@@ -1,1 +1,7 @@
 print("Hello, gol'rs.")
+
+# STEPS:
+# 1. find the dimensions of the terminal
+# 2. initialize the grid randomly
+# 3. write the nextgencompute function according to GOL rules
+# 4. infinite loop and run :)
